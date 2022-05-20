4571db6fd513a44bf273bdaec0ec87111cba12df
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-05-20 20:20:48 +0500

    fixed bug

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
 from . import views
 
-schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
     path('', views.home, name='home'),
